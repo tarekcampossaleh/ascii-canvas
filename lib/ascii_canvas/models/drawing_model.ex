10430d8a48f7da742e985dda3a89b4f7cc7bd616
tarekcampossaleh <tarekcampossaleh@gmail.com>
2022-01-31 10:28:44 -0300

    fix(drawing): fix edge case when y overflow canvas

--- lib/ascii_canvas/models/drawing_model.ex
+++ lib/ascii_canvas/models/drawing_model.ex
@@ -5,27 +5,29 @@ defmodule AsciiCanvas.Drawingmodel do
   import AsciiCanvas.CanvasModel
 
   defp insert_char_at(canvas, x, y, char) do
-    # TODO: fix bug when y overflows canvas size
     row =
       Enum.at(canvas, y)
       |> String.split("", trim: true)
       |> List.replace_at(x, char)
       |> Enum.join("")
 
-    canvas = List.replace_at(canvas, y, row)
-    canvas
+    List.replace_at(canvas, y, row)
   end
 
   defp write_canvas(canvas, width, height, x, y, outline, fill) do
     Enum.reduce(x..(width + x), canvas, fn i, acc ->
       Enum.reduce(y..(height + y), acc, fn j, inc ->
-        if i == x or i == x + width or j == y or j == y + height do
-          insert_char_at(inc, i, j, outline)
+        if j > 24 do
+          inc
         else
-          if fill == "" do
-            inc
+          if i == x or i == x + width or j == y or j == y + height do
+            insert_char_at(inc, i, j, outline)
           else
-            insert_char_at(inc, i, j, fill)
+            if fill == "" do
+              inc
+            else
+              insert_char_at(inc, i, j, fill)
+            end
           end
         end
       end)
