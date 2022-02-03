6160a1e31de9567993fe2184db065cde20b3334a
tarekcampossaleh <tarekcampossaleh@gmail.com>
2022-02-03 20:35:16 -0300

    fix(live_view): fixed live view fetch

--- lib/ascii_canvas_web/controllers/show_canvas_controller.ex
+++ lib/ascii_canvas_web/controllers/show_canvas_controller.ex
@@ -4,9 +4,18 @@ defmodule AsciiCanvasWeb.ShowCanvasController do
   alias AsciiCanvas.CanvasModel
 
   def mount(params, _session, socket) do
+    if connected?(socket), do: Process.send_after(self(), :update, 2000)
+
     canvas = CanvasModel.get_canvas(params["id"])
     canvas = %{id: canvas.id, value: Enum.join(canvas.value, "\n")}
     {:ok, assign(socket, :canvas, canvas)}
+  end
+
+  def handle_info(:update, socket) do
+    Process.send_after(self(), :update, 2000)
+    canvas = CanvasModel.get_canvas(socket.assigns.canvas.id)
+    canvas = %{id: canvas.id, value: Enum.join(canvas.value, "\n")}
+    {:noreply, assign(socket, :canvas, canvas)}
   end
 
   def render(assigns) do
